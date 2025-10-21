e547f771a0af1c1acd21662d8ac5291ea2c6e211
Gelo <cookiecupcakemain@gmail.com>
2025-10-21 09:28:33 +0800

    Delete gelo directory

--- gelo/resources/views/components/note-card.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <h3>{{ $title }}</h3>
-    <p>{{ $content }}</p>
-    <small>{{ $timestamp }}</small>
-</div>
--- gelo/routes/web.php
+++
@@ -1,5 +0,0 @@
-<?php
-use App\Http\Controllers\NoteController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/notes', [NoteController::class, 'index']);
